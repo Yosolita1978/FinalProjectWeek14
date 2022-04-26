c2ba47e5c588580c3170f62553817f58bbc0f35b
Cristina Rodriguez <yosola@gmail.com>
2022-04-26 15:45:51 -0700

    Api Call to the weather working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Students from './components/students';
+import Weather from './components/weather';
+
 
 function App() {
   return (
     <div className="App">
       Hello from Techtonica!
       <Students />
+      <Weather />
     </div>
   );
 }
