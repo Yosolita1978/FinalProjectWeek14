ac363a9ce5276e46242db8bfdee098cae540b8f3
Cristina Rodriguez <yosola@gmail.com>
2022-05-01 20:14:35 -0700

    Fixxing errors

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,8 +3,8 @@ const Login = ({user}) =>{
     return (
         <nav>
         <ul>
-            {!user ? (<li> <a href="http://localhost:3001/login">Login</a></li>) 
-            : (<li> Hello, {user.nickname} <a href="http://localhost:3001/logout">Logout</a></li>)}
+            {!user ? (<li> <a href="/login">Login</a></li>) 
+            : (<li> Hello, {user.nickname} <a href="/logout">Logout</a></li>)}
         </ul>
       </nav>
     )
